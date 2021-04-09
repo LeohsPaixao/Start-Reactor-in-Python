bbf7f4f061a54288930d48c3f7e97575cc402dda
Leonardo Paixão <leonardohspaixao@gmail.com>
2021-04-08 22:23:04 -0300

    almost ready audio function

--- config.py
+++ config.py
@@ -1,21 +1,34 @@
 import pygame
-
-computerCombination: []
-playerCombination: []
-computerCombinationPosition: 1
-combinationMaxPosition: 5
-memoryMaxCombination: 9
+import os
 
 
-def startReactor():
+computerCombination = []
+playerCombination = []
+computerCombinationPositions = 1
+combinationMaxPositions = 5
+memoryMaxCombination = 9
 
-    audio: {
-        'start': pygame.mixer.music('audio/start.mp3'),
-        'fail': pygame.mixer.music('audio/fail.mp3'),
-        'complete': pygame.mixer.music('audio/complete.mp3'),
-        'combinations': [pygame.mixer.music('audio/first.mp3', 'audio/second.mp3', 'audio/third.mp3', 'audio/fourth.mp3', 'audio/fiveth.mp3', 'audio/sixth.mp3', 'audio/seven.mp3', 'audio/eighth.mp3', 'audio/nineth.mp3')]
-    }
 
-    inteface: {
+class audio():
 
-    }
+    start = pygame.mixer.music.load(os.path.join('audio', 'start.mp3'))
+    fail = pygame.mixer.music.load(os.path.join('audio', 'fail.mp3'))
+    complete = pygame.mixer.music.load(os.path.join('audio', 'complete.mp3'))
+    combinations = [
+        pygame.mixer.music.load(os.path.join('audio', 'first.mp3', 'second.mp3', 'third.mp3',
+                                'fourth.mp3', 'fifth.mp3', 'sixth.mp3', 'seventh.mp3', 'eighth.mp3', 'nineth.mp3'))
+    ]
+
+    def loadAudio(self, filename):
+        self.file = open(filename)
+        self.audio = open(self.file)
+        audio.load()
+        return audio
+
+
+class interface():
+    pass
+
+
+class startReactor():
+    pass
